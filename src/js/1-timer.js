3c1f7bf2911b0fef9913032e8af16d5f19dcf3ce
Stef <xkristianwilliamsx@gmail.com>
2025-02-05 12:56:34 +0200

    test

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,1 +1,18 @@
-console.log('timer');
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+console.log('snackbar');
+
+const options = {
+  enableTime: true, // Дозволяє вибір часу
+  time_24hr: true, // Встановлює 24-годинний формат часу
+  defaultDate: new Date(), // Встановлює поточну дату як значення за замовчуванням
+  minuteIncrement: 1, // Крок вибору хвилин (кожні 1 хвилина)
+  onClose(selectedDates) {
+    // Колбек, який викликається після закриття календаря
+    console.log(selectedDates[0]); // Виводить обрану дату та час
+  },
+};
+
+// Ініціалізація flatpickr
+flatpickr('#datetime-picker', options);
