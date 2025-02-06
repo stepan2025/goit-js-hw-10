a74fdf0e844dc64e92bf236be848764e9e5febea
stepan2025 <xkristianwilliamsx@gmail.com>
2025-02-06 12:57:56 +0000

    Deploying to gh-pages from @ stepan2025/goit-js-hw-10@683484b95074d71b173c0a7ed1126ce69a8a3e9b 🚀

--- 2-snackbar.js
+++ 2-snackbar.js
@@ -1,2 +1,2 @@
-import"./assets/styles-BnCbqBCk.js";console.log("snackbar");
+import"./assets/styles-C0Fcewhx.js";import{i}from"./assets/vendor-BbbuE1sJ.js";document.querySelector(".form").addEventListener("submit",function(t){t.preventDefault();const s=Number(t.target.elements.delay.value),o=t.target.elements.state.value;new Promise((e,r)=>{setTimeout(()=>{o==="fulfilled"?e(s):r(s)},s)}).then(e=>{i.success({title:"Success",message:`✅ Fulfilled promise in ${e}ms`,position:"topRight"})}).catch(e=>{i.error({title:"Error",message:`❌ Rejected promise in ${e}ms`,position:"topRight"})})});
 //# sourceMappingURL=2-snackbar.js.map
